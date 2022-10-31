a67f951aa20c9b5a915fe69d65315473be0f331e
Isuru Fernando <isuruf@gmail.com>
2022-10-31 17:03:50 -0500

    Fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-$PYTHON configure.py --enable-shared --no-use-shipped-boost --boost-python-libname=boost_python${PYVER//./}
+$PYTHON configure.py --enable-shared --no-use-shipped-boost --boost-python-libname=boost_python${PY_VER//./}
 
 $PYTHON -m pip install . -vv
