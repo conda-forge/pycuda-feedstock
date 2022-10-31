bcf034b22d28bc278612cb3cf4d747e67055335c
Isuru Fernando <isuruf@gmail.com>
2022-10-31 16:46:30 -0500

    boost-python-libname

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-$PYTHON configure.py --enable-shared --no-use-shipped-boost
+$PYTHON configure.py --enable-shared --no-use-shipped-boost --boost-python-libname=boost_python${PYVER//./}
 
 $PYTHON -m pip install . -vv
